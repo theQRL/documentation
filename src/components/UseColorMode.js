9edb0d871d90b700095c914bd48d8f2aafb5ce6f
fr1t2 <fr1t2@protonmail.com>
2023-07-10 12:47:00 -0600

    remove console logging

--- src/components/UseColorMode.js
+++ src/components/UseColorMode.js
@@ -7,20 +7,7 @@ const ImageSwitcher = ({lightImageSrc, darkImageSrc}) => {
   if (themeIs === "light"){
     isDarkTheme = false;
   }
-  console.log("ImageSwitcher" + ImageSwitcher)
-  console.log("themeIs = " + themeIs)
-  console.log("isDarkTheme = " + isDarkTheme)
-  console.log(isDarkTheme ? darkImageSrc : lightImageSrc)
-  console.log("lightImageSrc = " + lightImageSrc)
-  console.log("darkImageSrc = " + darkImageSrc)
   return (<img src={isDarkTheme ? darkImageSrc : lightImageSrc} alt={isDarkTheme ? "darkImageSrc" : "lightImageSrc"} />)
-   
-
-   //if (isDarkTheme === "dark") {
-     //return (<img src={darkImageSrc } alt="dark " />)
-  //} else {
-    
-  //};
 };
 
 
